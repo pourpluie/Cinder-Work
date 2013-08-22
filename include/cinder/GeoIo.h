9a61d53ac415421171fedbbb8cb2f0045e8aa414
Andrew Bell <cinder.afb@gmail.com>
2013-08-21 20:03:18 -0400

    Working Teapot

--- include/cinder/GeoIo.h
+++ include/cinder/GeoIo.h
@@ -26,6 +26,7 @@
 #include "cinder/Cinder.h"
 #include "cinder/Exception.h"
 #include "cinder/Vector.h"
+#include "cinder/Matrix.h"
 
 namespace cinder { namespace geo {
 
@@ -50,42 +51,6 @@ class Source {
 	static void	copyData( uint8_t srcDimensions, const float *srcData, size_t numElements, uint8_t dstDimensions, size_t dstStrideBytes, float *dstData );
 	static void	copyDataMultAdd( const float *srcData, size_t numElements, uint8_t dstDimensions, size_t dstStrideBytes, float *dstData, const Vec2f &mult, const Vec2f &add );
 	static void	copyDataMultAdd( const float *srcData, size_t numElements, uint8_t dstDimensions, size_t dstStrideBytes, float *dstData, const Vec3f &mult, const Vec3f &add );
-};
-
-class Cube : public Source {
-  public:
-	Cube();
-	
-	Cube&		colors() { mHasColor = true; return *this; }
-	Cube&		texCoords() { mHasTexCoord0 = true; return *this; }
-	Cube&		normals() { mHasNormals = true; return *this; }
-	Cube&		position( const Vec3f &pos ) { mPos = pos; return *this; }
-	Cube&		scale( const Vec3f &scale ) { mScale = scale; return *this; }
-	Cube&		scale( float s ) { mScale = Vec3f( s, s, s ); return *this; }
-  
-	virtual size_t		getNumVerts() const override { return 24; }
-	virtual Mode		getMode() const { return Mode::TRIANGLES; }
-	
-	virtual bool		hasAttrib( Attrib attr ) const override;
-	virtual bool		canProvideAttrib( Attrib attr ) const override;
-	virtual uint8_t		getAttribDims( Attrib attr ) const override;
-	virtual void		copyAttrib( Attrib attr, uint8_t dims, size_t stride, float *dest ) const override ;
-	
-	virtual size_t		getNumIndices() const override { return 36; }
-	virtual void		copyIndices( uint16_t *dest ) const override;
-	virtual void		copyIndices( uint32_t *dest ) const override;
-	
-	Vec3f		mPos, mScale;
-	bool		mHasColor;
-	bool		mHasTexCoord0;
-	bool		mHasNormals;
-	
-	static float	sVertices[24*3];
-	static float	sColors[24*3];
-	static float	sTexCoords[24*2];
-	static float	sNormals[24*3];	
-	
-	static uint16_t	sIndices[36];
 };
 
 class Rect : public Source {
@@ -118,6 +83,95 @@ class Rect : public Source {
 	static float	sNormals[4*3];
 };
 
+class Cube : public Source {
+  public:
+	Cube();
+	
+	Cube&		colors() { mHasColor = true; return *this; }
+	Cube&		texCoords() { mHasTexCoord0 = true; return *this; }
+	Cube&		normals() { mHasNormals = true; return *this; }
+	Cube&		position( const Vec3f &pos ) { mPos = pos; return *this; }
+	Cube&		scale( const Vec3f &scale ) { mScale = scale; return *this; }
+	Cube&		scale( float s ) { mScale = Vec3f( s, s, s ); return *this; }
+  
+	virtual size_t		getNumVerts() const override { return 24; }
+	virtual Mode		getMode() const { return Mode::TRIANGLES; }
+	
+	virtual bool		hasAttrib( Attrib attr ) const override;
+	virtual bool		canProvideAttrib( Attrib attr ) const override;
+	virtual uint8_t		getAttribDims( Attrib attr ) const override;
+	virtual void		copyAttrib( Attrib attr, uint8_t dims, size_t stride, float *dest ) const override ;
+	
+	virtual size_t		getNumIndices() const override { return 36; }
+	virtual void		copyIndices( uint16_t *dest ) const override;
+	virtual void		copyIndices( uint32_t *dest ) const override;
+
+  protected:	
+	Vec3f		mPos, mScale;
+	bool		mHasColor;
+	bool		mHasTexCoord0;
+	bool		mHasNormals;
+	
+	static float	sVertices[24*3];
+	static float	sColors[24*3];
+	static float	sTexCoords[24*2];
+	static float	sNormals[24*3];	
+	
+	static uint16_t	sIndices[36];
+};
+
+class Teapot : public Source {
+  public:
+	Teapot();
+	
+	Teapot&		texCoords() { mHasTexCoord0 = true; return *this; }
+	Teapot&		normals() { mHasNormals = true; return *this; }
+	Teapot&		position( const Vec3f &pos ) { mPos = pos; return *this; }
+	Teapot&		scale( const Vec3f &scale ) { mScale = scale; return *this; }
+	Teapot&		scale( float s ) { mScale = Vec3f( s, s, s ); return *this; }
+	Teapot&		grid( int g ) { mGrid = g; return *this; }
+  
+	virtual size_t		getNumVerts() const override;
+	virtual Mode		getMode() const { return Mode::TRIANGLES; }
+	
+	virtual bool		hasAttrib( Attrib attr ) const override;
+	virtual bool		canProvideAttrib( Attrib attr ) const override;
+	virtual uint8_t		getAttribDims( Attrib attr ) const override;
+	virtual void		copyAttrib( Attrib attr, uint8_t dims, size_t stride, float *dest ) const override ;
+
+	virtual size_t		getNumIndices() const override;
+	virtual void		copyIndices( uint16_t *dest ) const override;
+	virtual void		copyIndices( uint32_t *dest ) const override;
+
+  protected:
+	void			calculate() const;
+  
+	static void		generatePatches( float *v, float *n, float *tc, uint32_t *el, int grid );
+	static void		buildPatchReflect( int patchNum, float *B, float *dB, float *v, float *n, float *tc, unsigned int *el,
+                                    int &index, int &elIndex, int &tcIndex, int grid, bool reflectX, bool reflectY );
+	static void		buildPatch( Vec3f patch[][4], float *B, float *dB, float *v, float *n, float *tc, 
+								unsigned int *el, int &index, int &elIndex, int &tcIndex, int grid, const Matrix33f reflect, bool invertNormal );
+	static void		getPatch( int patchNum, Vec3f patch[][4], bool reverseV );
+	static void		computeBasisFunctions( float *B, float *dB, int grid );
+	static Vec3f	evaluate( int gridU, int gridV, const float *B, const Vec3f patch[][4] );
+	static Vec3f	evaluateNormal( int gridU, int gridV, const float *B, const float *dB, const Vec3f patch[][4] );
+
+	int			mGrid;
+	Vec3f		mPos, mScale;
+	bool		mHasTexCoord0;
+	bool		mHasNormals;
+
+	mutable bool						mCalculationsCached;
+	mutable	int32_t						mNumVertices;
+	mutable int32_t						mNumIndices;
+	mutable std::unique_ptr<float>		mVertices;
+	mutable std::unique_ptr<float>		mTexCoords;
+	mutable std::unique_ptr<float>		mNormals;	
+	mutable std::unique_ptr<uint32_t>	mIndices;
+	
+	static const uint8_t	sPatchIndices[][16];
+	static const float		sCurveData[][3];
+};
 
 class Exc : public Exception {
 };
