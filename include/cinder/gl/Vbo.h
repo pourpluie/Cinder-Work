95423924d56552173ac45d0aa62bb0eb0ef7e946
Andrew Bell <cinder.afb@gmail.com>
2013-12-14 21:47:29 -0500

    Convenience Vbo constructor

--- include/cinder/gl/Vbo.h
+++ include/cinder/gl/Vbo.h
@@ -10,6 +10,9 @@ class Vbo : public BufferObj {
   public:
 	static VboRef	create( GLenum target );
 	static VboRef	create( GLenum target, GLsizeiptr allocationSize, const void *data = NULL, GLenum usage = GL_STATIC_DRAW );
+	template<typename T>
+	static VboRef	create( GLenum target, const std::vector<T> &v, GLenum usage = GL_STATIC_DRAW )
+		{ return create( target, v.size() * sizeof(T), v.data(), usage ); }
 	
   protected:
 	Vbo( GLenum target );
