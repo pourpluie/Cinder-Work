4e48d08e950f4276b2f2173355494e9a085108ce
Andrew Bell <cinder.afb@gmail.com>
2014-01-11 23:11:06 -0500

    Added cubemapping mipmapping, and defaulting min filter to GL_LINEAR_MIPMAP_LINEAR when mipmapping enabled

--- src/cinder/gl/Texture.cpp
+++ src/cinder/gl/Texture.cpp
@@ -74,6 +74,9 @@ void TextureBase::initParams( Format &format, GLint defaultInternalFormat )
 #if ! defined( CINDER_GLES )
 	glTexParameteri( mTarget, GL_TEXTURE_WRAP_R, format.mWrapR );
 #endif // ! defined( CINDER_GLES )
+
+	if( format.mMipmapping && ! format.mMinFilterSpecified )
+		format.mMinFilter = GL_LINEAR_MIPMAP_LINEAR;
 	glTexParameteri( mTarget, GL_TEXTURE_MIN_FILTER, format.mMinFilter );
 	glTexParameteri( mTarget, GL_TEXTURE_MAG_FILTER, format.mMagFilter );
 	
@@ -1067,6 +1070,9 @@ TextureCubeMap::TextureCubeMap( const Surface8u images[6], Format format )
 	for( GLenum target = 0; target < 6; ++target )
 		glTexImage2D( GL_TEXTURE_CUBE_MAP_POSITIVE_X + target, 0, mInternalFormat, images[target].getWidth(), images[target].getHeight(), 0,
 			( images[target].hasAlpha() ) ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, images[target].getData() );
+			
+	if( format.mMipmapping ) 
+		glGenerateMipmap( mTarget );			
 }
 
 /////////////////////////////////////////////////////////////////////////////////
