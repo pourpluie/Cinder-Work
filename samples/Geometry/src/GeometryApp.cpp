8dd6538faf88b15bdcddabecf78a7052fcf779e4
Paul Houx <github@cowlumbus.nl>
2014-02-02 19:31:02 +0100

    Cleaned up the Geometry sample.

--- samples/Geometry/src/GeometryApp.cpp
+++ samples/Geometry/src/GeometryApp.cpp
@@ -40,6 +40,7 @@ private:
 	uint8_t				mSubdivision;
 	bool				mWireframe;
 	bool				mColored;
+	bool				mShowNormals;
 
 	CameraPersp			mCamera;
 	MayaCamUI			mMayaCam;
@@ -47,8 +48,7 @@ private:
 	gl::VertBatchRef	mGrid;
 
 	gl::VboMeshRef		mPrimitive;
-	gl::VboMeshRef		mOriginalNormals;
-	gl::VboMeshRef		mCalculatedNormals;
+	gl::VboMeshRef		mNormals;
 
 	gl::GlslProgRef		mWireframeShader;
 
@@ -61,6 +61,7 @@ void GeometryApp::setup()
 	mSubdivision = 1;
 	mWireframe = true;
 	mColored = true;
+	mShowNormals = false;
 
 	//
 	gl::Texture::Format fmt;
@@ -104,7 +105,7 @@ void GeometryApp::setup()
 	createShader();
 	createPrimitive();
 
-	mCamera.setEyePoint( Vec3f(0, 2, 4) );
+	mCamera.setEyePoint( Vec3f(3, 4, 6) );
 	mCamera.setCenterOfInterestPoint( Vec3f(0, 0, 0) );
 
 	mWireframeShader->uniform( "uTexture", 0 );
@@ -112,9 +113,6 @@ void GeometryApp::setup()
 
 void GeometryApp::update()
 {
-	float t = static_cast<float>( getElapsedSeconds() );
-
-	Vec3f p( 5.0f * sinf(t), 5.0f * cos(t), 5.0f * sin(t * 0.7f) );
 }
 
 void GeometryApp::draw()
@@ -128,36 +126,28 @@ void GeometryApp::draw()
 		if(mGrid)
 			mGrid->draw();
 
-		if(mOriginalNormals)
-			gl::draw( mOriginalNormals );
-
-		if(mCalculatedNormals)
-			gl::draw( mCalculatedNormals );
+		if(mNormals && mShowNormals)
+			gl::draw( mNormals );
 	}
 
 	if(mPrimitive)
 	{
-		try {
-			gl::TextureBindScope textureBindScope( mTexture );
+		gl::TextureBindScope textureBindScope( mTexture );
 
-			gl::GlslProgScope glslProgScope( mWireframe ? mWireframeShader :
-				gl::context()->getStockShader( gl::ShaderDef().color().texture( mTexture ) ) );
+		gl::GlslProgScope glslProgScope( mWireframe ? mWireframeShader :
+			gl::context()->getStockShader( gl::ShaderDef().color().texture( mTexture ) ) );
 			
-			gl::enableAlphaBlending();
-			gl::enable( GL_CULL_FACE );
+		gl::enableAlphaBlending();
+		gl::enable( GL_CULL_FACE );
 
-			glCullFace( GL_FRONT );
-			gl::draw( mPrimitive );
+		glCullFace( GL_FRONT );
+		gl::draw( mPrimitive );
 
-			glCullFace( GL_BACK );
-			gl::draw( mPrimitive );
+		glCullFace( GL_BACK );
+		gl::draw( mPrimitive );
 		
-			gl::disable( GL_CULL_FACE );
-			gl::disableAlphaBlending();
-		}
-		catch( const std::exception &e ) {
-			console() << e.what() << std::endl;
-		}
+		gl::disable( GL_CULL_FACE );
+		gl::disableAlphaBlending();
 	}
 }
 
@@ -199,12 +189,15 @@ void GeometryApp::keyDown( KeyEvent event )
 			mSubdivision--;
 		createPrimitive();
 		break;
-	case KeyEvent::KEY_w:
-		mWireframe = !mWireframe;
-		break;
 	case KeyEvent::KEY_c:
 		mColored = !mColored;
 		createPrimitive();
+		break;
+	case KeyEvent::KEY_n:
+		mShowNormals = !mShowNormals;
+		break;
+	case KeyEvent::KEY_w:
+		mWireframe = !mWireframe;
 		break;
 	}
 }
@@ -233,7 +226,7 @@ void GeometryApp::createPrimitive(void)
 		primitive = geom::SourceRef( new geom::Icosahedron( geom::Icosahedron() ) );
 		break;
 	case ICOSPHERE:
-		primitive = geom::SourceRef( new geom::Icosphere( geom::Icosphere().subdivision( mSubdivision ) ) );
+		primitive = geom::SourceRef( new geom::Icosphere( geom::Icosphere().subdivision(3) ) );
 		break;
 	case SPHERE:
 		primitive = geom::SourceRef( new geom::Sphere( geom::Sphere() ) );
@@ -259,116 +252,109 @@ void GeometryApp::createPrimitive(void)
 	}
 
 	mPrimitive = gl::VboMesh::create( mesh );
-/*
-	AxisAlignedBox3f bounds = mesh.calcBoundingBox();
-	console() << "WxHxD = " << bounds.getSize().x << "x" << bounds.getSize().y << "x" << bounds.getSize().z << std::endl;
-		
-	mOriginalNormals = gl::VboMesh::create( DebugMesh( mesh, Color(1,1,0) ) );
-	mesh.recalculateNormals(false);
-	mCalculatedNormals = gl::VboMesh::create( DebugMesh( mesh, Color(0,1,1) ) );
-*/
+	mNormals = gl::VboMesh::create( DebugMesh( mesh, Color(1,1,0) ) );
 }
 
 void GeometryApp::createShader(void)
 {
 	try {
-	mWireframeShader = gl::GlslProg::create( gl::GlslProg::Format()
-		.vertex(
-			"#version 150\n"
-			"\n"
-			"uniform mat4	ciModelViewProjection;\n"
-			"in vec4		ciPosition;\n"
-			"in vec4		ciColor;\n"
-			"in vec2		ciTexCoord0;\n"
-			"\n"
-			"out VertexData {\n"
-			"	vec4 color;\n"
-			"	vec2 texcoord;\n"
-			"} vVertexOut;\n"
-			"\n"
-			"void main(void) {\n"
-			"	vVertexOut.color = ciColor;\n"
-			"	vVertexOut.texcoord = ciTexCoord0;\n"
-			"	gl_Position = ciModelViewProjection * ciPosition;\n"
-			"}\n"
-		) 
-		.geometry(
-			"#version 150\n"
-			"\n"
-			"layout (triangles) in;\n"
-			"layout (triangle_strip, max_vertices = 3) out;\n"
-			"\n"
-			"uniform vec2 			uViewportSize;\n"
-			"\n"
-			"in VertexData	{\n"
-			"	vec4 color;\n"
-			"	vec2 texcoord;\n"
-			"} vVertexIn[];\n"
-			"\n"
-			"out VertexData	{\n"
-			"	noperspective vec3 distance;\n"
-			"	vec4 color;\n"
-			"	vec2 texcoord;\n"
-			"} vVertexOut;\n"
-			"\n"
-			"void main(void)\n"
-			"{\n"
-			"	// taken from 'Single-Pass Wireframe Rendering'\n"
-			"	vec2 p0 = uViewportSize * gl_in[0].gl_Position.xy / gl_in[0].gl_Position.w;\n"
-			"	vec2 p1 = uViewportSize * gl_in[1].gl_Position.xy / gl_in[1].gl_Position.w;\n"
-			"	vec2 p2 = uViewportSize * gl_in[2].gl_Position.xy / gl_in[2].gl_Position.w;\n"
-			"\n"
-			"	vec2 v0 = p2-p1;\n"
-			"	vec2 v1 = p2-p0;\n"
-			"	vec2 v2 = p1-p0;\n"
-			"	float fArea = abs(v1.x*v2.y - v1.y * v2.x);\n"
-			"\n"
-			"	vVertexOut.distance = vec3(fArea/length(v0),0,0);\n"
-			"	vVertexOut.color = vVertexIn[0].color;\n"
-			"	vVertexOut.texcoord = vVertexIn[0].texcoord;\n"
-			"	gl_Position = gl_in[0].gl_Position;\n"
-			"	EmitVertex();\n"
-			"\n"
-			"	vVertexOut.distance = vec3(0,fArea/length(v1),0);\n"
-			"	vVertexOut.color = vVertexIn[1].color;\n"
-			"	vVertexOut.texcoord = vVertexIn[1].texcoord;\n"
-			"	gl_Position = gl_in[1].gl_Position;\n"
-			"	EmitVertex();\n"
-			"\n"
-			"	vVertexOut.distance = vec3(0,0,fArea/length(v2));\n"
-			"	vVertexOut.color = vVertexIn[2].color;\n"
-			"	vVertexOut.texcoord = vVertexIn[2].texcoord;\n"
-			"	gl_Position = gl_in[2].gl_Position;\n"
-			"	EmitVertex();\n"
-			"\n"
-			"	EndPrimitive();\n"
-			"}\n"
-		) 
-		.fragment(
-			"#version 150\n"
-			"\n"
-			"uniform sampler2D uTexture;\n"
-			"\n"
-			"in VertexData	{\n"
-			"	noperspective vec3 distance;\n"
-			"	vec4 color;\n"
-			"	vec2 texcoord;\n"
-			"} vVertexIn;\n"
-			"\n"
-			"out vec4				oColor;\n"
-			"\n"
-			"void main(void) {\n"
-			"	// determine frag distance to closest edge\n"
-			"	float fNearest = min(min(vVertexIn.distance[0],vVertexIn.distance[1]),vVertexIn.distance[2]);\n"
-			"	float fEdgeIntensity = exp2(-1.0*fNearest*fNearest);\n"
-			"\n"
-			"	// blend between edge color and face color\n"
-			"	vec4 vFaceColor = texture( uTexture, vVertexIn.texcoord ) * vVertexIn.color; vFaceColor.a = 0.85;\n"
-			"	vec4 vEdgeColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
-			"	oColor = mix(vFaceColor, vEdgeColor, fEdgeIntensity);\n"
-			"}\n"
-		)
-	);
+		mWireframeShader = gl::GlslProg::create( gl::GlslProg::Format()
+			.vertex(
+				"#version 150\n"
+				"\n"
+				"uniform mat4	ciModelViewProjection;\n"
+				"in vec4		ciPosition;\n"
+				"in vec4		ciColor;\n"
+				"in vec2		ciTexCoord0;\n"
+				"\n"
+				"out VertexData {\n"
+				"	vec4 color;\n"
+				"	vec2 texcoord;\n"
+				"} vVertexOut;\n"
+				"\n"
+				"void main(void) {\n"
+				"	vVertexOut.color = ciColor;\n"
+				"	vVertexOut.texcoord = ciTexCoord0;\n"
+				"	gl_Position = ciModelViewProjection * ciPosition;\n"
+				"}\n"
+			) 
+			.geometry(
+				"#version 150\n"
+				"\n"
+				"layout (triangles) in;\n"
+				"layout (triangle_strip, max_vertices = 3) out;\n"
+				"\n"
+				"uniform vec2 			uViewportSize;\n"
+				"\n"
+				"in VertexData	{\n"
+				"	vec4 color;\n"
+				"	vec2 texcoord;\n"
+				"} vVertexIn[];\n"
+				"\n"
+				"out VertexData	{\n"
+				"	noperspective vec3 distance;\n"
+				"	vec4 color;\n"
+				"	vec2 texcoord;\n"
+				"} vVertexOut;\n"
+				"\n"
+				"void main(void)\n"
+				"{\n"
+				"	// taken from 'Single-Pass Wireframe Rendering'\n"
+				"	vec2 p0 = uViewportSize * gl_in[0].gl_Position.xy / gl_in[0].gl_Position.w;\n"
+				"	vec2 p1 = uViewportSize * gl_in[1].gl_Position.xy / gl_in[1].gl_Position.w;\n"
+				"	vec2 p2 = uViewportSize * gl_in[2].gl_Position.xy / gl_in[2].gl_Position.w;\n"
+				"\n"
+				"	vec2 v0 = p2-p1;\n"
+				"	vec2 v1 = p2-p0;\n"
+				"	vec2 v2 = p1-p0;\n"
+				"	float fArea = abs(v1.x*v2.y - v1.y * v2.x);\n"
+				"\n"
+				"	vVertexOut.distance = vec3(fArea/length(v0),0,0);\n"
+				"	vVertexOut.color = vVertexIn[0].color;\n"
+				"	vVertexOut.texcoord = vVertexIn[0].texcoord;\n"
+				"	gl_Position = gl_in[0].gl_Position;\n"
+				"	EmitVertex();\n"
+				"\n"
+				"	vVertexOut.distance = vec3(0,fArea/length(v1),0);\n"
+				"	vVertexOut.color = vVertexIn[1].color;\n"
+				"	vVertexOut.texcoord = vVertexIn[1].texcoord;\n"
+				"	gl_Position = gl_in[1].gl_Position;\n"
+				"	EmitVertex();\n"
+				"\n"
+				"	vVertexOut.distance = vec3(0,0,fArea/length(v2));\n"
+				"	vVertexOut.color = vVertexIn[2].color;\n"
+				"	vVertexOut.texcoord = vVertexIn[2].texcoord;\n"
+				"	gl_Position = gl_in[2].gl_Position;\n"
+				"	EmitVertex();\n"
+				"\n"
+				"	EndPrimitive();\n"
+				"}\n"
+			) 
+			.fragment(
+				"#version 150\n"
+				"\n"
+				"uniform sampler2D uTexture;\n"
+				"\n"
+				"in VertexData	{\n"
+				"	noperspective vec3 distance;\n"
+				"	vec4 color;\n"
+				"	vec2 texcoord;\n"
+				"} vVertexIn;\n"
+				"\n"
+				"out vec4				oColor;\n"
+				"\n"
+				"void main(void) {\n"
+				"	// determine frag distance to closest edge\n"
+				"	float fNearest = min(min(vVertexIn.distance[0],vVertexIn.distance[1]),vVertexIn.distance[2]);\n"
+				"	float fEdgeIntensity = exp2(-1.0*fNearest*fNearest);\n"
+				"\n"
+				"	// blend between edge color and face color\n"
+				"	vec4 vFaceColor = texture( uTexture, vVertexIn.texcoord ) * vVertexIn.color; vFaceColor.a = 0.85;\n"
+				"	vec4 vEdgeColor = vec4(1.0, 1.0, 1.0, 0.85);\n"
+				"	oColor = mix(vFaceColor, vEdgeColor, fEdgeIntensity);\n"
+				"}\n"
+			)
+		);
 	}
 	catch( const std::exception& e ) {
 		console() << e.what() << std::endl;
