7cd44c15bcdae9dbfc26ee6efd1da8b898f1b8cf
Rich E <rich.eakin@gmail.com>
2014-03-06 20:37:34 -0500

    added TouchEvent::Touch::setPos()

--- include/cinder/app/TouchEvent.h
+++ include/cinder/app/TouchEvent.h
@@ -46,6 +46,8 @@ class TouchEvent : public Event {
 		float		getY() const { return mPos.y; }		
 		//! Returns the position of the touch measured in points
 		Vec2f		getPos() const { return mPos; }
+		//! Sets the position of the touch measured in points
+		void		setPos( const Vec2i &pos )	{ mPos = pos; }
 		//! Returns the previous x position of the touch measured in points
 		float		getPrevX() const { return mPrevPos.x; }
 		//! Returns the previous y position of the touch measured in points
